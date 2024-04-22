8a018ad633f41c932a9dfa8a0705d900fd03c4a6
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-04-22 00:11:31 -0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } , if: -> { email}
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 end
