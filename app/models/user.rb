dfbc57a3280cd2d06ac7c768468d3c08c2c47bae
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-04-22 00:29:06 -0300

    Add comment model & Migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } , if: -> { email}
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, if: -> { email }
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
   has_many :posts
+  has_many :comments
 end
